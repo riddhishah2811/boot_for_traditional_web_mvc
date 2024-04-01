20b2b5dc36b3cf9e6426db7cdf5f45330a535617
RIDDHI SHAH <riddhishah2222@gmail.com>
2024-04-01 21:37:07 +0530

    Solution for traditional web mvc repo

---
+++ weshopify-commerce-mvc-boot/src/main/java/com/weshopify/retailcommerce/WeshopifyCommerceMvcBootApplication.java
@@ -0,0 +1,13 @@
+package com.weshopify.retailcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeshopifyCommerceMvcBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeshopifyCommerceMvcBootApplication.class, args);
+	}
+
+}
---
+++ weshopify-commerce-mvc-boot/src/test/java/com/weshopify/retailcommerce/WeshopifyCommerceMvcBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weshopify.retailcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeshopifyCommerceMvcBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
